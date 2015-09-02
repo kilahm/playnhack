9b450a5865d5efd5442957bd7e1b4aa1038481f2
Isaac Leinweber <brotchen@gmail.com>
2015-09-02 15:55:50 +0000

    Only non-global code is type-checked.

--- public/index.php
+++ public/index.php
@@ -2,15 +2,20 @@
 
 include dirname(__DIR__) . '/src/app.hh';
 
-$app = new App();
+function run() {
 
-//Vector<string>
-$message = Vector {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D'};
+    $app = new App();
 
-echo "I would expect this should fail: ". $app->run($message, 99) . '<br />';
+    //Vector<string>
+    $message = Vector {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D'};
+
+    echo "I would expect this should fail: ". $app->run($message, 99) . '<br />';
 
 
-echo "This should pass: " . $app->runCorrect($message, 99) . '<br />';
+    echo "This should pass: " . $app->runCorrect($message, 99) . '<br />';
 
 
-echo "So why do both work...?";
+    echo "So why do both work...?";
+}
+
+run();
